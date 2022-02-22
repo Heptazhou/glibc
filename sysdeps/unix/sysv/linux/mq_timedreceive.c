6899c76ab88b08204e94d5984cb74c2458354dfd
Heptazhou <zhou@0h7z.com>
2022-02-22 08:52:14 +0000

    Update mq_timedreceive.c

--- sysdeps/unix/sysv/linux/mq_timedreceive.c
+++ sysdeps/unix/sysv/linux/mq_timedreceive.c
@@ -39,8 +39,8 @@ ___mq_timedreceive_time64 (mqd_t mqdes, char *__restrict msg_ptr, size_t msg_len
 		     && !in_int32_t_range (abs_timeout->tv_sec);
   if (need_time64)
     {
-      int r = SYSCALL_CANCEL (mq_timedreceive_time64, mqdes, msg_ptr, msg_len,
-			      msg_prio, abs_timeout);
+      int r = -1; // SYSCALL_CANCEL (mq_timedreceive_time64, mqdes, msg_ptr, msg_len, msg_prio, abs_timeout);
+      errno = ENOSYS;
       if (r >= 0 || errno != ENOSYS)
 	return r;
       __set_errno (EOVERFLOW);
